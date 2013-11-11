a65bdd0ec0bdf8a007ae05b0a0d1319a3e07da3e
Louis-Ye <yeluyupt@gmail.com>
2013-11-11 15:02:25 -0800

    Initialize CMPT300 P2

---
+++ CMPT300P2/scheduling.cpp
@@ -0,0 +1,42 @@
+
+
+void Processor() {
+	while(1) {
+		// system kernal space (short term scheduler)
+		Process pro = fetch process from ready queue;
+
+		int whichScheduler = 0;
+		
+		//User space
+		for (int i=0; i < runningTime(pro); i++) {
+			if (pro.executeOneCommand() != BLOCK);
+			else {
+				whichScheduler = 1;
+				break;
+			}
+		}
+
+		// system kernal space
+		if (whichScheduler == 0) {
+			put pro into ready queue;	//short term scheduler behavior
+		}
+		else if (whichScheduler == 1) {
+			put pro into block queue;	//mid term scheduler behavior
+		}
+	}
+}
+
+void midTern() { //interrupt of the block end
+	while (1) {
+		for (Process pro : block queue) {
+			if (pro received io) {
+				put pro into ready queue;
+			}
+		}
+	}
+}
+
+
+void longTerm() {
+
+}
