6c50d9665e3147c3819416aec6efb0de3c157b49
Louis-Ye <yeluyupt@gmail.com>
2013-11-12 15:36:09 -0800

    add content of header.h

--- CMPT300P2/scheduling.cpp
+++ CMPT300P2/scheduling.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <pthread.h>
+#include "header.h"
 
 using namespace std;
-
-int const NO_INTTERUPT = 1;
-int const MID_TERM_TO_BQ = 2;
-int const MID_TERM_BQ_TO_RQ = 3;
-int const LONG_TERM_TO_RQ = 4;
-int const LONG_TERM_EXIT = 5;
 
 void masterProcessor() {
 	while(1) {
