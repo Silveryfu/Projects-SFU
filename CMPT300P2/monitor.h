bff962a0ad1df32121c7e6da3558d41eb542d4ac
Silvery Fu <silveryfu@gmail.com>
2013-11-13 17:13:55 -0800

    monitor.h added.

---
+++ CMPT300P2/monitor.h
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#define synchronized(M) for(Lock M##_lock=M; M##_lock; M##_lock.setUnlock())
+
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+
+class Lock
+{
+public:
+	Lock(pthread_mutex_t &mutex):m_mutex(mutex),m_locked(true)
+	{
+		pthread_mutex_lock(&mutex);
+	}
+
+	~Lock()
+	{
+	 	pthread_mutex_unlock(&m_mutex);
+	}
+
+	operator bool() const
+	{
+		return m_locked;
+	}
+
+	void setUnlock()
+	{
+		m_locked = false;
+	}
+
+private:
+	pthread_mutex_t &m_mutex;
+        bool m_locked;
+};
