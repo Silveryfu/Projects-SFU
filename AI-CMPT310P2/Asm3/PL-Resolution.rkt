e5e64b23cb04032ed0f6eae293560f965061f61f
Silvery Fu <silveryfu@gmail.com>
2014-03-13 23:43:37 -0700

    Complete resolution.

--- AI-CMPT310P2/Asm3/PL-Resolution.rkt
+++ AI-CMPT310P2/Asm3/PL-Resolution.rkt
@@ -23,30 +23,42 @@
     (when (equal? i new_clause) (set! flag #t)) flag)
   flag)
 
-(define (PL-Resolve C1 C2)
-  (define resolvent '())   ; the resolvent shall be initialized empty
-  (for ([i C1])
+(define (PL-Resolve C1 C2)   
+  (define resolvent '(#f))   ; the resolvent shall be initialized #f, to represent the case no resolvent
+  (for ([i C1])              
     (for ([j C2])
-      #:break (not (null? resolvent))
-      (cond [(complementary-literals? i j)
+      #:break (not (equal? '(#f) resolvent))
+      (cond [(complementary-literals? i j) ;once find the complementary pair, stop iteration
             (set! resolvent (remove-duplicates (append (remove i C1) (remove j C2))))]
             [else #t])))
   resolvent)
-                
-;(define (PL-Resolution KB alpha)
- ; (define clauses (append KB alpha))
-  ;(loop clauses))
+
+(define (PL-Resolution KB alpha)
+  (define clauses (append KB alpha))
+  (loop clauses))
  
-;(define (loop clauses)
-  ;(cond [(contain-empty? clauses) #t]
-        ;[
-
-
-
+(define (loop clauses)
+  (define L (length clauses))
+  (define resolvent '(#f))
+  (cond [(contain-empty? clauses) #t]
+        [else (begin 
+                (for ([i (in-range L)])
+                  (for ([j (in-range (add1 i) L)])
+                    #:break (not (equal? '(#f) resolvent))
+                    (set! resolvent (PL-Resolve (list-ref clauses i) (list-ref clauses j))) 
+                    (cond [(exist? clauses resolvent) (set! resolvent '(#f))]  ; if already exist, ignore 
+                          [else (when (not (equal? resolvent '(#f)))           ; and reset resolvent
+                                  (set! clauses (append (list resolvent) clauses)))])))
+                (if (equal? resolvent '(#f)) #f (loop clauses)))]))
+                
+               
 ;unit tests
-
 ;(contain-empty? KB_CNF)
-;(PL-Resolution KB_CNF (list-ref queries_N_CNF 0))
-(PL-Resolve '(~a b d c ~d) '(~a d ~d e f))
 ;(displayln KB_CNF)
+;(PL-Resolve '(~a) '(a))
 ;(equal? '(FirstGrade) (list-ref KB_CNF 0))
+(PL-Resolution KB_CNF (list-ref queries_N_CNF 0))
+(PL-Resolution KB_CNF (list-ref queries_N_CNF 1))
+(PL-Resolution KB_CNF (list-ref queries_N_CNF 2))
+(PL-Resolution KB_CNF (list-ref queries_N_CNF 3))
+(PL-Resolution KB_CNF (list-ref queries_N_CNF 4))
